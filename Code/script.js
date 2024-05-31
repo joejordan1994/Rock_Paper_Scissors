78910282311cb9cd32c4e745dfafa33d353229d6
spcymshrm <joe.jordan1994@gmail.com>
2024-05-31 15:13:23 -0400

    Milestone
    Written out variables, and three functions to be able to get the computer and human choice, still working on being able to compare the values from each function

--- Code/script.js
+++ Code/script.js
@@ -1,21 +1,23 @@
-/* Write a program that allows a user to decide between rock, paper, or scissors. 
-Also create a random index for the computer to randomly choose between rock, paper, or scissors. 
-Each time that both the computer and the user have made their selection, 
-compare the two choices to determine the winner and keep score by adding 1 point to either the computer or the user. 
-The first player to 5 points wins, then the game resets. */
-
-/*When the user inputs their choice, compare it to the random choice made by the computer to determine
-who wins the first point*/
-
-
-/*The desired output is a declaration of who the winner is, followed by a score update*/
-
-/*When a user inputs which choice they want to make, run a random choice generator for the computer to determine
-the computer's choice. Decide the winner by a set of rules for what beats what*/
+const choices = [`rock`, `paper`, `scissors`];
 
 function getComputerChoice() {
-    const choices = [`rock`, `paper`, `scissors`];
-    const randomChoice = choices[Math.floor(Math.random() * 3)];
-    console.log(randomChoice);
+  let randomChoice = choices[Math.floor(Math.random() * 3)];
+  console.log(randomChoice);
 }
 
+getComputerChoice();
+
+function getHumanChoice() {
+  let humanChoice = prompt(
+    `Please choose either rock, paper, or scissors.`
+  ).toLowerCase();
+  console.log(humanChoice);
+}
+
+getHumanChoice();
+
+if (randomChoice == humanChoice) {
+  console.log(`It's a tie!`);
+} else {
+  console.log(`It's not a tie, one of y'all definitely won`);
+}
