4ac347fedcf2837a46a2571ed1b31996c1ee1e90
spcymshrm <joe.jordan1994@gmail.com>
2024-05-31 13:07:27 -0400

    More pseudocode

--- Code/script.js
+++ Code/script.js
@@ -4,3 +4,12 @@ Each time that both the computer and the user have made their selection,
 compare the two choices to determine the winner and keep score by adding 1 point to either the computer or the user. 
 The first player to 5 points wins, then the game resets. */
 
+/*When the user inputs their choice, compare it to the random choice made by the computer to determine
+who wins the first point*/
+
+
+/*The desired output is a declaration of who the winner is, followed by a score update*/
+
+/*When a user inputs which choice they want to make, run a random choice generator for the computer to determine
+the computer's choice. Decide the winner by a set of rules for what beats what*/
+
