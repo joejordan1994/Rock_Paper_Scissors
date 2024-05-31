f310ebadf58fa4631bb5c4e2d0fe5aeedf9fbfdf
spcymshrm <joe.jordan1994@gmail.com>
2024-05-31 12:59:00 -0400

    Wrote out pseudocode to plan for coding

---
+++ Code/script.js
@@ -0,0 +1,6 @@
+/* Write a program that allows a user to decide between rock, paper, or scissors. 
+Also create a random index for the computer to randomly choose between rock, paper, or scissors. 
+Each time that both the computer and the user have made their selection, 
+compare the two choices to determine the winner and keep score by adding 1 point to either the computer or the user. 
+The first player to 5 points wins, then the game resets. */
+
